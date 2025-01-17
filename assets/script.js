1db5ddee2ed7853ef1ab65bed049c1738d1968a3
Omarespo-dev <omaresposito21@gmail.com>
2025-01-17 18:45:31 +0100

    finish all

--- assets/script.js
+++ assets/script.js
@@ -3,37 +3,37 @@ const teamMembers = [
     name: "Marco Bianchi",
     role: "Designer",
     email: "marcobianchi@team.com",
-    img: "img/male1.png"
+    img: "/assets/img/male1.png"
   },
   {
     name: "Laura Rossi",
     role: "Front-end Developer",
     email: "laurarossi@team.com",
-    img: "img/female1.png"
+    img: "/assets/img/female1.png"
   },
   {
     name: "Giorgio Verdi",
     role: "Back-end Developer",
     email: "giorgioverdi@team.com",
-    img: "img/male2.png"
+    img: "/assets/img/male2.png"
   },
   {
     name: "Marta Ipsum",
     role: "SEO Specialist",
     email: "martarossi@team.com",
-    img: "img/female2.png"
+    img: "/assets/img/female2.png"
   },
   {
     name: "Roberto Lorem",
     role: "SEO Specialist",
     email: "robertolorem@team.com",
-    img: "img/male3.png"
+    img: "/assets/img/male3.png"
   },
   {
     name: "Daniela Amet",
     role: "Analyst",
     email: "danielaamet@team.com",
-    img: "img/female3.png"
+    img: "/assets/img/female3.png"
   }
 ];
 
@@ -46,31 +46,34 @@ const teamMembers = [
 // DOM ELEMENT
 const output =document.querySelector(".output")
 
-// PRENDO IL NOME I RUOLI EMAIL E IMG DAGLI ARRAY
+
+// Dichiaro una stringa dove all'interno includero la parte di html
 let stringa = ""
 
+// PRENDO IL NOME I RUOLI EMAIL E IMG DAGLI ARRAY
 for (let i = 0; i < teamMembers.length; i++) {
   // TEAM CARD 
   let teamIesimo = teamMembers[i]
-
+  
+  // In questo caso uso il Destructuring dove  vado a prendermi il nome il ruolo etcc...
   const { name, role, email, img } = teamIesimo
-  console.log(name, role, email, img)
+  // console.log(name, role, email, img)
 
   stringa += `
-    <div class="card mb-3" style="max-width: 540px;">
-            <div class="row g-0">
+    <div class="card mb-5 col-4 bg-black text-white p-0 " style="max-width: 540px; ">
+            <div class="row g-2 ">
               <div class="col-md-4">
                 <img src="${img}" class="img-fluid rounded-start" alt="${name}">
               </div>
               <div class="col-md-8">
                 <div class="card-body">
-                  <h5 class="card-title">${name}</h5>
+                  <h3 class="card-title">${name}</h3>
                   <p class="card-text">${role}</p>
-                  <p class="card-text"><small class="text-body-secondary">${email}</small></p>
+                  <p class="card-text text-primary "><a href="#" class="text-decoration-none">${email}</a></p>
                 </div>
               </div>
             </div>
-      </div>
+    </div>
   
   `
   output.innerHTML = stringa
@@ -79,5 +82,8 @@ for (let i = 0; i < teamMembers.length; i++) {
 
 
 
-// const {name, role, email, img} = teamMembers
-// console.log(name,role,email,img)
+
+// SI POTREBBE FARE ANCHE CON FOREACH
+// teamMembers.forEach(function(teamIesimo){
+//   console.log(teamIesimo)
+// }) 
